52fd2f9c1e3eaa9d6acfe585c8bcf56bdef6cfe4
Sreetama Sarkar <sreetama1994@gmail.com>
2023-09-07 23:16:39 -0700

    making bad pixel injection for efficient for higher error rates

--- scripts/bad_pixels.py
+++ scripts/bad_pixels.py
@@ -4,6 +4,13 @@ import random
 import rawpy
 import sys
 
+def generate_bad_pixel(value, delta):
+    if (1 + delta) * value > 1023:
+        delta_range = [(0, int((1 - delta) * value))]
+    else:
+        delta_range = [(0, int((1 - delta) * value)), (int((1 + delta) * value), 1023)]
+    return random.randint(*random.choice(delta_range))
+
 def decide_range(value, delta):
     if (1 + delta) * value > 1023:
         return [(0, int((1 - delta) * value))]
@@ -11,16 +18,20 @@ def decide_range(value, delta):
         return [(0, int((1 - delta) * value)), (int((1 + delta) * value), 1023)]
 
 if __name__ == "__main__":
-    data_dir = '/data1/Invertible_ISP'
-    org_dir = f'{data_dir}/original_images'
-    imgs_dir = f'{data_dir}/Invertible_ISP_0.7/imgs'
-    masks_dir = f'{data_dir}/Invertible_ISP_0.7/masks'
+    delta = 0.7
+    bad_rate = 0.01
+    data_dir = f'/data1/Bad_Pixel_Detection/data/Invertible_ISP_{delta}_{bad_rate}'
+    org_dir = '/data1/Invertible_ISP/original_images'
+    # data_dir = f'/data1/Bad_Pixel_Detection/data/ISP_{delta}_{bad_rate}'
+    # org_dir = '/data1/S7-ISP-Dataset/medium_dng'
+
+    imgs_dir = f'{data_dir}/imgs'
+    masks_dir = f'{data_dir}/masks'
     if not os.path.exists(masks_dir):
         os.makedirs(masks_dir)
     if not os.path.exists(imgs_dir):
         os.makedirs(imgs_dir)
-    delta = 0.7
-    bad_rate = 0.70
+    
     
     err, crt = 0, 0
     for i, dng in enumerate(os.listdir(org_dir)):
@@ -37,19 +48,44 @@ if __name__ == "__main__":
         H, W = raw_data.shape
         bad_num = int(bad_rate * H * W)
         random.seed(i)
-        bad_pos = [(random.randint(0, H - 1), random.randint(0, W - 1)) for _ in range(bad_num)]
+        
+        # bad_pos = [(random.randint(0, H - 1), random.randint(0, W - 1)) for _ in range(bad_num)]
         mask = np.zeros((H, W))
-        for x, y in bad_pos:
-            o_val = raw_data[x][y]
-            ranges = decide_range(o_val, delta)
-            chosed_range = random.choice(ranges)
-            b_val = random.randint(*chosed_range)
-            mask[x][y] = 1
-            raw_data[x][y] = b_val
+        
+        # Calculate the total number of pixels in the image
+        total_pixels = H * W
+
+        # Calculate the number of pixels to modify (70% of total_pixels)
+        pixels_to_modify = int(0.7 * total_pixels)
+        
+        # Randomly select pixel indices to modify
+        pixel_indices = np.random.choice(total_pixels, size=pixels_to_modify, replace=False)
+
+        # Split the pixel indices into x and y coordinates
+        y_indices, x_indices = np.unravel_index(pixel_indices, (H, W))
+
+        # Get the current pixel values for the selected pixels
+        current_pixel_values = raw_data[y_indices, x_indices]
+
+        # Generate bad pixel values based on the current pixel values using the function
+        bad_pixel_values = np.vectorize(generate_bad_pixel)(current_pixel_values, delta)
+
+        # Clip the bad pixel values to ensure they are within the valid range (0-255)
+        # bad_pixel_values = np.clip(bad_pixel_values, 0, 255)
+
+        # Apply the generated bad pixel values to the selected pixels
+        raw_data[y_indices, x_indices] = bad_pixel_values
+        mask[y_indices, x_indices] = 1
+
+        # for x, y in bad_pos:
+        #     o_val = raw_data[x][y]
+        #     ranges = decide_range(o_val, delta)
+        #     chosed_range = random.choice(ranges)
+        #     b_val = random.randint(*chosed_range)
+        #     mask[x][y] = 1
+        #     raw_data[x][y] = b_val
         np.save(os.path.join(imgs_dir, file_name), raw_data)
         np.save(os.path.join(masks_dir, file_name), mask)
         print(f'noisy {dng} is saved.')
     print(f"Bad pixels injection finished. {crt} success, {err} fail.")      
             
-        
-    
